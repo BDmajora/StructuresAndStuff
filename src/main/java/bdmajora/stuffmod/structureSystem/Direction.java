8840dbe83c0903e2930f4b1ed3aaa4ec2e73bcf8
bdmajora <bdmajora@protonmail.com>
2025-08-06 14:06:18 -0600

    New Direction system

---
+++ src/main/java/bdmajora/stuffmod/structureSystem/Direction.java
@@ -0,0 +1,164 @@
+package bdmajora.stuffmod.structureSystem;
+
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.util.helper.Axis;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.WorldSource;
+import bdmajora.stuffmod.structureSystem.BlockPosition;
+import net.minecraft.core.util.phys.Vec3;
+
+public enum Direction {
+	/**
+	 * EAST, 5, X
+	 */
+	X_POS(new BlockPosition(1, 0, 0), 5, "EAST", Axis.X),
+	/**
+	 * WEST, 4, X
+	 */
+	X_NEG(new BlockPosition(-1, 0, 0), 4, "WEST", Axis.X),
+	/**
+	 * UP, 1, Y
+	 */
+	Y_POS(new BlockPosition(0, 1, 0), 1, "UP", Axis.Y),
+	/**
+	 * DOWN, 0, Y
+	 */
+	Y_NEG(new BlockPosition(0, -1, 0), 0, "DOWN", Axis.Y),
+	/**
+	 * SOUTH, 3, Z
+	 */
+	Z_POS(new BlockPosition(0, 0, 1), 3, "SOUTH", Axis.Z),
+	/**
+	 * NORTH, 2, Z
+	 */
+	Z_NEG(new BlockPosition(0, 0, -1), 2, "NORTH", Axis.Z);
+
+	private final BlockPosition vec;
+	private Direction opposite;
+	private final int side;
+	private final String name;
+	private final Axis axis;
+
+	Direction(BlockPosition vec3I, int side, String name, Axis axis) {
+		this.vec = vec3I;
+		this.side = side;
+		this.name = name;
+		this.axis = axis;
+	}
+
+	public TileEntity getTileEntity(WorldSource world, TileEntity tile) {
+		BlockPosition pos = new BlockPosition(tile.x + vec.x, tile.y + vec.y, tile.z + vec.z);
+		return world.getTileEntity(pos.x, pos.y, pos.z);
+	}
+
+	public Block getBlock(WorldSource world, TileEntity tile) {
+		BlockPosition pos = new BlockPosition(tile.x + vec.x, tile.y + vec.y, tile.z + vec.z);
+		return world.getBlock(pos.x, pos.y, pos.z);
+	}
+
+	public Block getBlock(WorldSource world, BlockPosition baseVec) {
+		BlockPosition pos = new BlockPosition(baseVec.x + vec.x, baseVec.y + vec.y, baseVec.z + vec.z);
+		return world.getBlock(pos.x, pos.y, pos.z);
+	}
+
+	public TileEntity getTileEntity(WorldSource world, BlockPosition baseVec) {
+		BlockPosition pos = new BlockPosition(baseVec.x + vec.x, baseVec.y + vec.y, baseVec.z + vec.z);
+		return world.getTileEntity(pos.x, pos.y, pos.z);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Direction getOpposite() {
+		return opposite;
+	}
+
+	public BlockPosition getVec() {
+		return vec.copy();
+	}
+
+	public Axis getAxis() {
+		return axis;
+	}
+
+	public static Direction getDirectionFromSide(int side) {
+		for (Direction dir : values()) {
+			if (dir.side == side) {
+				return dir;
+			}
+		}
+		return Direction.X_NEG;
+	}
+
+	public static Direction getFromName(String name) {
+		for (Direction dir : values()) {
+			if (dir.name.equalsIgnoreCase(name)) {
+				return dir;
+			}
+		}
+		return null;
+	}
+
+	public Direction rotate(int amount) {
+		if (this == Y_POS || this == Y_NEG) return this;
+		return getDirectionFromSide(net.minecraft.core.util.helper.Direction.getDirectionById(this.side).rotate(amount).getId());
+	}
+
+	/**
+	 * Gets minecraft's side number, NOTE: this and .ordinal() aren't the same!
+	 *
+	 * @return Minecraft's side number.
+	 */
+	public int getSideNumber() {
+		return side;
+	}
+
+	public Side getSide() {
+		return Side.getSideById(side);
+	}
+
+	public Direction shiftAxis() {
+		switch (this) {
+			case X_POS:
+				return Direction.Z_POS;
+			case X_NEG:
+				return Direction.Z_NEG;
+			case Z_POS:
+				return Direction.X_POS;
+			case Z_NEG:
+				return Direction.X_NEG;
+		}
+		return this;
+	}
+
+	public Vec3 getMinecraftVec() {
+		return Vec3.getPermanentVec3(vec.x, vec.y, vec.z);
+	}
+
+	public static net.minecraft.core.util.helper.Direction getOriginalDirection(Direction direction) {
+		switch (direction) {
+			case Z_NEG:
+				return net.minecraft.core.util.helper.Direction.NORTH;
+			case Z_POS:
+				return net.minecraft.core.util.helper.Direction.SOUTH;
+			case X_NEG:
+				return net.minecraft.core.util.helper.Direction.WEST;
+			case X_POS:
+				return net.minecraft.core.util.helper.Direction.EAST;
+			default:
+				return net.minecraft.core.util.helper.Direction.NONE;
+		}
+	}
+
+	static {
+		X_POS.opposite = X_NEG;
+		X_NEG.opposite = X_POS;
+		Y_NEG.opposite = Y_POS;
+		Y_POS.opposite = Y_NEG;
+		Z_NEG.opposite = Z_POS;
+		Z_POS.opposite = Z_NEG;
+	}
+
+}
